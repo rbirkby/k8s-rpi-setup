3fadf7c2a36fc4b8461d0f3018183d25f28c8df5
Richard Birkby <rbirkby@gmail.com>
2018-03-14 20:48:34 +0000

    Use etc/hosts for network-local name resolution

--- utils/run-local-registry.sh
+++ utils/run-local-registry.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -ex
 
 export DOCKER_HOST=localhost
-docker run -d -p 5000:5000 -e "REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io" --restart=always --name registry registry:2
+docker run -d -p 5000:5000 -e "REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io" --restart=always --name caching-registry registry:2
+docker run -d -p 5001:5001 --restart=always --name local-registry registry:2
